65b58b417f5faa26d2b54c2710a2c232a9207f5f
devLarissaRodrigues <devlarissarodrigues@gmail.com>
2022-05-26 14:03:34 -0300

    desafio_1

---
+++ desafio_1.py
@@ -0,0 +1,10 @@
+# 1. Peça ao seu usuário para inserir 3 dados: nome, idade e cidade.
+
+# 2. Imprima no console uma única mensagem desejando boas vindas ao usuário.
+
+
+nome = input("Olá! Por favor, digite seu nome: ")
+idade = int(input("Digite sua idade: "))
+cidade = input("Digita a sua cidade: ")
+
+print(f"Olá, {nome}! Boas vindas!!! Fico feliz por você estar falando da cidade {cidade}. Bom saber saber que você tem {idade} anos.")
